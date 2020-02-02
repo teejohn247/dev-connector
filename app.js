6d8d20d69da49e7fdf242b60bc471d7e6c00ac8d
teejohn247 <teejohn247@gmail.com>
2020-02-02 18:31:59 +0100

    'final'

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import connectDb from './config/db';
 import userRouter from './routes/user';
+import path from 'path';
+
 
 const app = express();
 dotenv.config();
@@ -22,11 +24,20 @@ connectDb();
 
 app.get('/api/v1', (req, res) => {
     res.json({
-      message: 'Welcome to Auto-Mart API'
+      message: 'Welcome to Dev-Connector API'
     });
   });
 
 app.use('/api/v1', userRouter);
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
